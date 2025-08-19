a8baecb474b22ed69051465e88b3f67612cc22c9
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-08-19 21:14:15 +0000

    Exercício 01a Resolvido

--- ex01a.py
+++ ex01a.py
@@ -1,1 +1,7 @@
 # Implemente um programa que leia um número inteiro e mostre seu sucessor e seu antecessor.
+numero = int(input())
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print(f'{sucessor} é o sucessor e {antecessor} é o antecessor de {numero}.')
