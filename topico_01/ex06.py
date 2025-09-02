958f6822c3dd45112837aa98c2ad412746724e92
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-09-02 17:22:55 -0300

    Update ex06.py

--- topico_01/ex06.py
+++ topico_01/ex06.py
@@ -1,2 +1,11 @@
-# Crie um programa que leia dois números e uma operação (soma, subtração, multiplicação ou divisão)
-# do usuário. Realize a operação correspondente e mostre o resultado.
+# Crie um programa que leia dois números e uma
+# operação (soma, subtração, multiplicação ou divisão)
+# do usuário. Realize a operação correspondente
+# e mostre o resultado.
+
+numero1 = int(input('Digite o primeiro numero: '))
+numero2 = int(input('Digite o segundo numero: '))
+operador = input('digite o operador [+, -, *, /]')
+exp = f'{numero1} {operador} {numero2}'
+resultado = eval(exp) # evitar usar.
+print(resultado)
