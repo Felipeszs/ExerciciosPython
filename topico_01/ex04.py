d8135f9e0c9024b042960b3a39c5fbc7e06f23da
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-09-02 17:22:10 -0300

    Update ex04.py

--- topico_01/ex04.py
+++ topico_01/ex04.py
@@ -1,1 +1,6 @@
-# Faça um programa que leia o preço de um produto e mostre o valor com 5% de desconto.
+# Faça um programa que leia o preço de um produto
+# e mostre o valor com 5% de desconto.
+
+price = float(input('Digite o valor do produto: '))
+
+print(f'O valor com desconto é {round(price * 0.95, 2)}')
