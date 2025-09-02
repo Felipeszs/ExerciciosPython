8bee3331a95409a4f4e93b14d5555fa8d0818dee
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-09-02 17:21:36 -0300

    Update ex02.py

--- topico_01/ex02.py
+++ topico_01/ex02.py
@@ -1,1 +1,12 @@
-# Implemente um programa que leia 4 notas de um aluno e mostre a média aritmética.
+# Implemente um programa que leia 4 notas de um aluno
+# e mostre a média aritmética.
+
+nota1 = float(input('Digite a nota 1: '))
+nota2 = float(input('Digite a nota 2: '))
+nota3 = float(input('Digite a nota 3: '))
+nota4 = float(input('Digite a nota 4: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'A média final é {media:.2f}.')
+print(f'A média final é {round(media,2)}.')
