6e6a212397a93467f915247794ad95dfefb4382b
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-09-02 17:21:51 -0300

    Update ex03.py

--- topico_01/ex03.py
+++ topico_01/ex03.py
@@ -1,1 +1,12 @@
-# Supondo que queremos pintar o muro da instituição, faça um programa que leia a altura e a largura do muro e mostre a quantidade de tinta necessária (1 litro de tinta pode pintar uma área de 2 metros quadrados).
+# Supondo que queremos pintar o muro da instituição, faça um programa
+# que leia a altura e a largura do muro e mostre a quantidade de tinta
+# necessária (1 litro de tinta pode pintar uma área de 2 metros quadrados).
+
+altura = int(input('Digite a altura: '))
+largura = int(input('Digite a largura: '))
+
+area = altura * largura
+
+quantidade_tinta = area / 2
+
+print(f'Você utilizará {quantidade_tinta} litros de tinta.')
