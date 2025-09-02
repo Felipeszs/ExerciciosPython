2d614406e96a4447532075769c7e3a040aee44f5
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2025-09-02 17:22:31 -0300

    Update ex05.py

--- topico_01/ex05.py
+++ topico_01/ex05.py
@@ -1,1 +1,15 @@
-# Implemente um programa que tenha como entrada o valor em uma reais e mostre o valor correspondente em dólar.
+# Implemente um programa que tenha como entrada o valor em
+# reais e mostre o valor correspondente em dólar.
+import requests
+
+URL = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+
+response = requests.get(URL)
+COTACAO = response.json()
+COTACAO = float(COTACAO.get('USDBRL').get('bid'))
+
+reais = float(input('Digite o valor em reais: '))
+
+dolares = round(reais / COTACAO, 2)
+
+print(f'{reais} corresponde a {dolares} dolares.')
