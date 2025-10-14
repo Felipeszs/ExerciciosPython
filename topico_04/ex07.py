47f1c7da167f96cc9578105514aae5d2317eb86e
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2025-10-14 15:24:15 -0300

    Exercício Tupla

---
+++ topico_04/ex07.py
@@ -0,0 +1,6 @@
+# Usando tupla, resolva:
+# Crie um programa que receba uma
+# data como string no formato dd/mm/aaaa.
+# Em seguida, retorne a data por extenso.
+# Por exemplo, dada a entrada "14/10/2025",
+# retorne "14 de outubro de 2025".
