f5f3546ad63afc99dad69654a31a27928948716d
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-10-21 13:57:59 -0300

    Exercícios de dicionário

---
+++ topico_05/ex02.py
@@ -0,0 +1,6 @@
+# Uma técnica muito comum para apresentar uma Word Cloud é
+# contar a frequência de palavras em um texto para destacar
+# a fonte de cada palavra. Para isso, crie um programa que
+# leia uma string, por exemplo "Python é incrível e Python
+# é simples", e retorne o dicionário: {'Python': 2, 'é': 2,
+# 'e': 1, 'incrível': 1, 'simples': 1}.
